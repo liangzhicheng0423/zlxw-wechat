d1c1589b1e6b7009fb8d7d356a7c3cc59b1d4def
zhicheng.liang <zhicheng.liang@shihe.tech>
2024-06-07 19:34:12 +0800

    test 3

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,15 +4,15 @@ module.exports = {
   /** 大括号内的首尾需要空格 */
   bracketSpacing: true,
   /** 箭头函数，只有一个参数的时候，不需要括号 */
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   /** 不需要自动在文件开头插入 @prettier */
   insertPragma: false,
   /** jsx 不使用单引号，而使用双引号 */
   jsxSingleQuote: false,
   /** 使用默认的折行标准 */
-  proseWrap: "preserve",
+  proseWrap: 'preserve',
   /** 对象的 key 仅在必要时用引号 */
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   /** 不需要写文件开头的 @prettier */
   requirePragma: false,
   /** 行尾需要有分号 */
@@ -20,34 +20,35 @@ module.exports = {
   /** 使用单引号 */
   singleQuote: true,
   /** 末尾不需要逗号 */
-  trailingComma: "none",
+  trailingComma: 'none',
   /** 一行使用一个属性 */
   singleAttributePerLine: true,
   /** 维护现有行结尾 */
-  endOfLine: "auto",
+  endOfLine: 'auto',
   /** tailwind config */
-  tailwindConfig: "./tailwind.config.js",
+
+  htmlWhitespaceSensitivity: 'ignore',
 
   /** import sort */
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   importOrder: [
-    "^vite",
-    "^react",
-    "^classnames",
-    "^antd",
-    "@fepkg/(.*)$",
-    "<THIRD_PARTY_MODULES>",
-    "^@packages/(.*)$",
-    "^@/common/(.*)$",
-    "^@/components/(.*)$",
-    "^@/layouts/(.*)$",
-    "^@/localdb/(.*)$",
-    "^@/router(.*)$",
-    "^@/pages/(.*)$",
-    "^@/types/(.*)$",
-    "^[./].*(?<!\\.(c|le|sc)ss)$",
-    "\\.(c|le|sc)ss$",
+    '^vite',
+    '^react',
+    '^classnames',
+    '^antd',
+    '@fepkg/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@packages/(.*)$',
+    '^@/common/(.*)$',
+    '^@/components/(.*)$',
+    '^@/layouts/(.*)$',
+    '^@/localdb/(.*)$',
+    '^@/router(.*)$',
+    '^@/pages/(.*)$',
+    '^@/types/(.*)$',
+    '^[./].*(?<!\\.(c|le|sc)ss)$',
+    '\\.(c|le|sc)ss$'
   ],
   importOrderSortSpecifiers: true,
-  importOrderGroupNamespaceSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true
 };
