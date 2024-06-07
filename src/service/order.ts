af32cc40a4f84024ec20bccd210f39c327c71fc6
zhicheng.liang <zhicheng.liang@shihe.tech>
2024-06-07 19:51:04 +0800

    test 4

--- src/service/order.ts
+++ src/service/order.ts
@@ -6,12 +6,14 @@ export const unifiedorder = async (req: any, res: any) => {
 
   console.log('message', message);
   const ip = req.headers['x-forwarded-for']; // 小程序直接callcontainer请求会存在
+  const openid = req.headers['x-wx-openid']; // 小程序直接callcontainer请求会存在
+
   const option = {
     body: '测试', // 订单描述
     out_trade_no: `WERUNMP_${Date.now()}`, // 自定义订单号
     sub_mch_id: '1678905103', // 微信支付商户号
     total_fee: 1, // 金额，单位：分
-    openid: message.FromUserName, // 用户唯一身份ID
+    openid: openid, // 用户唯一身份ID
     spbill_create_ip: ip, // 用户客户端IP地址
     env_id: req.headers['x-wx-env'], // 接收回调的环境ID
     callback_type: 2, // 云托管服务接收回调，填2
