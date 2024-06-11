6c551fdfa35d5ec344b88bea6f05dfe667ea6aa6
zhicheng.liang <zhicheng.liang@shihe.tech>
2024-06-11 22:36:09 +0800

    add test log

--- src/service/order.ts
+++ src/service/order.ts
@@ -70,19 +70,27 @@ export const unifiedorderCb = async (req: any, res: any) => {
   const userId = message.subOpenid;
 
   const tradeNo = message.outTradeNo;
+
+  console.log('--------- 1');
   const { level } = getLevelAndProduct(tradeNo);
+  console.log('--------- 2', level);
 
   if (message.resultCode !== 'SUCCESS' && message.returnCode !== 'SUCCESS') return;
+
+  console.log('--------- 3');
 
   let is_award = false;
 
   // 奖励其父用户
   const user = await User.findOne({ where: { user_id: userId } });
+
   const formatUser = user?.toJSON();
+  console.log('--------- 4', formatUser);
 
   if (formatUser?.p_id) {
     const shareUser = await User.findOne({ where: { user_id: formatUser.p_id } });
     const formatShareUser = shareUser?.toJSON();
+    console.log('--------- 4.5', formatShareUser);
 
     if (formatShareUser && !formatUser.is_award) {
       await award(formatShareUser.user_id, 'order');
@@ -102,6 +110,8 @@ export const unifiedorderCb = async (req: any, res: any) => {
     expire_date: getExpireDate(date, level as VipLevel),
     subscribe_status: true
   });
+
+  console.log('--------- 5', getExpireDate(date, level as VipLevel));
 
   await sendMessage(userId, '会员开通成功，请扫码添加客服，并向客服发送“激活”');
 
