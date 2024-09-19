fa4bd2fed82443af8e05a68324943d9818192584
zhicheng.liang <zhicheng.liang@shihe.tech>
2024-09-19 13:16:30 +0800

    update user list

--- src/service/getUserList.ts
+++ src/service/getUserList.ts
@@ -10,11 +10,12 @@ const getAccessToken = async () => {
       `http://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APP_ID}&secret=${APP_SECRET}`
     );
 
-    console.log('获取 access token ', res);
     const { access_token } = res.data as any;
+
+    console.log('access_token: ', access_token);
     return access_token;
   } catch (error) {
-    console.log('getAccessToken error', error);
+    console.log('getAccessToken error');
     return '';
   }
 };
@@ -24,10 +25,10 @@ export const getUserList = async (message: TextMessage) => {
     const access_token = getAccessToken();
     if (!access_token) return;
     const response = await axios.get(
-      `https://api.weixin.qq.com/cgi-bin/user/get?access_token=${access_token}&next_openid=oHBuD6awi6Jsb3cckL3f93VO1TZA`
+      `http://api.weixin.qq.com/cgi-bin/user/get?access_token=${access_token}&next_openid=oHBuD6awi6Jsb3cckL3f93VO1TZA`
     );
 
-    console.log(response.data);
+    console.log('获取用户列表：', response.data);
   } catch (error) {
     console.log('getUserList error', error);
   }
